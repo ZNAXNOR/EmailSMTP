9dbff36685e3a22594a9351d20b5a33c8ac3546c
Omkar Dalvi <121810601+ZNAXNOR@users.noreply.github.com>
2023-10-01 18:01:58 +0530

    Added View Models

---
+++ ViewModels/EmailViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmailSMTP.ViewModels
+{
+    public class EmailViewModel
+    {
+        //Email
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        //Subject
+        [Required]        
+        public string Subject { get; set; }
+
+        //Message
+        [Required]
+        public string Message { get; set; }
+    }
+}
