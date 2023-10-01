99e6c746e0b1a3b48ff9a530a35d71deb524f094
Omkar Dalvi <121810601+ZNAXNOR@users.noreply.github.com>
2023-10-01 18:06:28 +0530

    Added EmailController

---
+++ Controllers/EmailController.cs
@@ -0,0 +1,27 @@
+﻿using EmailSMTP.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmailSMTP.Controllers
+{
+    public class EmailController : Controller
+    {
+        private readonly IEmailSenderInterface emailSender;
+
+        public EmailController(IEmailSenderInterface emailSender)
+        {
+            this.emailSender = emailSender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(EmailViewModel emailViewModel)
+        {
+            await emailSender.emailViewModel(emailViewModel.Email, emailViewModel.Subject, emailViewModel.Message);
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
