fa45a2801e1845be38e98dcbc79f4ace0ca659ae
Omkar Dalvi <121810601+ZNAXNOR@users.noreply.github.com>
2023-10-01 17:17:24 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace EmailSMTP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
